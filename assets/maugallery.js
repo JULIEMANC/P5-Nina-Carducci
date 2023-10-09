3a8d361189d7190840dd141b3abec8310259e7f8
JULIEMANC <136808377+JULIEMANC@users.noreply.github.com>
2023-10-09 16:27:23 +0200

    changement des formats et taille image

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -235,11 +235,8 @@
       }
     },
     filterByTag() {
-      if ($(this).hasClass("active-tag")) {
-        return;
-      }
       $(".active-tag").removeClass("active active-tag");
-      $(this).addClass("active-tag");
+      $(this).addClass("active active-tag");
 
       var tag = $(this).data("images-toggle");
 
